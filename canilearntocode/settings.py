0db07f5418e1ecef3da5a4742c6cee6120ba6003
Robert Alford <robertsalford@gmail.com>
2017-04-29 15:33:55 -0700

    Adjust templates DIRS setting for project structure

--- canilearntocode/settings.py
+++ canilearntocode/settings.py
@@ -198,7 +198,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(PROJECT_ROOT, "templates")
+            os.path.join(PROJECT_ROOT, "canilearntocode", "common", "templates")
         ],
         "APP_DIRS": True,
         "OPTIONS": {
